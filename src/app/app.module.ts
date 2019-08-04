b45354f65f34dd55caa44ca31e8008f9d22d495a
Manuel Nucci <manuucci96@gmail.com>
2019-08-04 11:33:16 -0300

    Configured some settings concerning to indentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,17 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		HeroesComponent,
-		HeroDetailComponent
-	],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		FormsModule,
-	],
-	providers: [],
-	bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        HeroDetailComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,21 +3,21 @@ import { Hero } from '../hero';
 import { HEROES } from "../mock-heroes";
 
 @Component({
-	selector: 'app-heroes',
-	templateUrl: './heroes.component.html',
-	styleUrls: ['./heroes.component.css']
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-	
-	heroes = HEROES;
-	selectedHero: Hero;
-	
-	constructor() { }
-	
-	ngOnInit() {
-	}
-	
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-	}
+
+    heroes = HEROES;
+    selectedHero: Hero;
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
