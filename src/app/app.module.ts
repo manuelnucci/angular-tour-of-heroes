a7c38a4600f01d86c17c6bee488de96def42ac25
Manuel Nucci <manuucci96@gmail.com>
2019-08-08 23:08:47 -0300

    Tour of Heroes - Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HeroesComponent } from "./heroes/heroes.component";
-import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-    declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
-    imports: [BrowserModule, AppRoutingModule, FormsModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        HeroDetailComponent,
+        MessagesComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production
+        })
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { MessageService } from './message.service';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+    providedIn: 'root' // root level, it can be injected anywhere in the app.
+})
+export class HeroService {
+    constructor(private messageService: MessageService) {}
+
+    getHeroes(): Observable<Hero[]> {
+        // TODO: send the message _after_ fetching the heroes
+        this.messageService.add('HeroService: fetched heroes');
+        return of(HEROES);
+    }
+}
